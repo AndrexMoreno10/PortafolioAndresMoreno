1a06ea1c5de7fe68db34b92a8fa7d6be5ca6a8f3
AndrexMoreno10 <adp2333@gmail.com>
2026-04-18 02:42:17 -0500

    Commit  Fix header and add module hero

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { HeaderModule } from './header/header.module';
+import { HeroModule } from './hero/hero.module';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderModule],
+  imports: [HeaderModule, HeroModule],
   
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
